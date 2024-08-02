3b4193e323cb22ac640ead1422fd8b9deb0dff65
Axel Perez <152705784+axelprz@users.noreply.github.com>
2024-08-02 12:16:29 -0300

    integrating swing with spring boot

--- src/main/java/com/axelprz/book_store/BookStoreApplication.java
+++ src/main/java/com/axelprz/book_store/BookStoreApplication.java
@@ -1,13 +1,27 @@
 package com.axelprz.book_store;
 
+import com.axelprz.book_store.view.BookForm;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.awt.*;
 
 @SpringBootApplication
 public class BookStoreApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BookStoreApplication.class, args);
+		ConfigurableApplicationContext context =
+				new SpringApplicationBuilder(BookStoreApplication.class)
+						.headless(false)
+						.web(WebApplicationType.NONE)
+						.run(args);
+		EventQueue.invokeLater(()->{
+			BookForm bookForm = context.getBean(BookForm.class);
+			bookForm.setVisible(true);
+		});
 	}
 
 }
